ffe1405ff3c2cde1704eb9a3a3c540fc3eb6e1aa
al-amin90 <ijesun30@gmail.com>
2025-02-11 11:02:00 +0600

    event-emitter added

--- event-emitter.js
+++ event-emitter.js
@@ -1,1 +1,14 @@
 // -------------------- Event driven architecture --------------------
+const EventEmitter = require('events')
+const myEmitter = new EventEmitter()
+
+//listen
+myEmitter.on('birthday', () => {
+    console.log("happy birthday to you");
+})
+
+myEmitter.on('birthday', (gift) => {
+    console.log(`i will sent you ${gift}`);
+})
+
+myEmitter.emit('birthday', 'bike')
