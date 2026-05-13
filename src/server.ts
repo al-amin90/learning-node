8c72f6c69d0cc359e1677a80a58f834868fd5cf1
al-amin90 <ijesun30@gmail.com>
2026-05-13 14:59:40 +0600

    feat: Route Handling with HTTP Requests & Responses

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,21 @@
 import { createServer, IncomingMessage, Server } from "http";
 
 const server: Server = createServer((req: IncomingMessage, res) => {
-  console.log(req);
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/" && method === "GET") {
+    // console.log("this is root route");
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(JSON.stringify({ message: "this is root route" }));
+  } else if (url === "/product" && method === "GET") {
+    // console.log("this is root route");
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(JSON.stringify({ message: "this is product route" }));
+  } else {
+    res.writeHead(404, { "content-type": "text/plain" });
+    res.end("Route is not found");
+  }
 });
 
 server.listen(5000, () => {
