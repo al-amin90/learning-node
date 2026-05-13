91cf1d7b27dc1262000fcccb220fb5535be9be2b
al-amin90 <ijesun30@gmail.com>
2026-05-13 16:24:10 +0600

    feat: Building GET Method Operations

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,19 +1,41 @@
 import type { IncomingMessage, ServerResponse } from "http";
 import { productService } from "../service/product.service";
+import type { IProduct } from "../types/product.type";
 
-const productController = (req: IncomingMessage, res: ServerResponse) => {
+const productController = async (req: IncomingMessage, res: ServerResponse) => {
   const url = req.url;
   const method = req.method;
 
+  const urlParts = url?.split("/");
+  console.log("urlParts", urlParts);
+
+  const id =
+    urlParts && urlParts[1] === "products" ? Number(urlParts[2]) : null;
+
+  console.log("id", id);
+
+  //   get all products
   if (url === "/products" && method === "GET") {
-    const data = [{ id: 43, category: "silver egale" }];
-    const products = productService.readProduct();
+    const products = await productService.readProduct();
+
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "Products Retrived Successfully",
+        data: products,
+      }),
+    );
+  } else if (method === "GET" && id !== null) {
+    //   Get Single Product
+    const products = await productService.readProduct();
+
+    const product = products.find((d: IProduct) => d.id === id);
 
     res.writeHead(200, { "content-type": "application/json" });
     res.end(
       JSON.stringify({
         message: "Product Retrived Successfully",
-        data: products,
+        data: product,
       }),
     );
   }
--- src/routes/route.ts
+++ src/routes/route.ts
@@ -9,7 +9,7 @@ const routeHandler = (req: IncomingMessage, res: ServerResponse) => {
     // console.log("this is root route");
     res.writeHead(200, { "content-type": "application/json" });
     res.end(JSON.stringify({ message: "this is root route" }));
-  } else if (url === "/products") {
+  } else if (url?.startsWith("/products")) {
     productController(req, res);
   } else {
     res.writeHead(404, { "content-type": "text/plain" });
